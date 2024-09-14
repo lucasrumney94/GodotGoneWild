a85adc43f4545eb26738f6c00853a8bd2d3daeff
Jamblefoot <32472250+Jamblefoot@users.noreply.github.com>
2024-09-13 21:24:53 -0700

    guy can move
    move can guy

--- godot-wild-tower/Objects/Player/player.gd
+++ godot-wild-tower/Objects/Player/player.gd
@@ -4,7 +4,19 @@ extends CharacterBody3D
 @export var tilt_lower_limit: float = deg_to_rad((-89))
 @export var tilt_upper_limit: float = deg_to_rad(89)
 
+@export var SPEED = 5.0
+@export var JUMP_VELOCITY = 4.5
+
 var mouse_rotation: Vector3
+
+var is_jumping: bool = false
+
+var acceleration: float = 5
+var current_acceleration: float = 0.0
+var speed_mult: float = 1.0
+var last_velocity: Vector3
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _ready():
@@ -28,6 +40,111 @@ func _unhandled_input(event: InputEvent):
 		update_camera(get_process_delta_time(), rotation_input, tilt_input)
 
 
+func _physics_process(delta):
+	var on_floor: bool = is_on_floor()
+	#if ladder != null:
+		#on_floor = true
+	#CHECK IF SHOULD DO HEAD BOB ON LANDING
+	#if on_floor:
+		#on_steps = false
+		#if !was_on_floor && !on_steps:# && last_velocity.y < -5.0: #JUST LANDED
+			##print("PLAYING IMPACT AT Y VELOCITY OF " + str(last_velocity.y))
+			#if last_velocity.y < -20.0:
+				#landing_head_bob = true
+			#play_impact(-last_velocity.y)
+	
+	# Add the gravity.
+	if !on_floor:# && !is_clambering:
+		velocity.y -= gravity * delta
+		last_velocity = velocity
+		if is_jumping && velocity.y < 0:
+			is_jumping = false
+		#if on_steps && velocity.y < -5:
+			#on_steps = false
+	#else:
+		#if crouched_up:
+			#animation_player.play("crouch_reset")
+			#GameEvents.emit_player_crouch(false)
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and on_floor:
+		velocity.y = JUMP_VELOCITY
+		is_jumping = true
+		on_floor = false
+		
+	#was_on_floor = on_floor
+	
+	#if !crouch_toggle:
+		#if is_crouching:
+			#if !Input.is_action_pressed("crouch") || ladder != null:
+				#try_uncrouch()
+		#else:
+			#if Input.is_action_just_pressed("crouch") && ladder == null:# and is_on_floor():
+				#crouch()
+	#else:
+		#if is_crouching && (crouch_toggled_off || ladder != null):
+			#try_uncrouch()
+			
+	
+	
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	#if input_dir.y > 0:
+		#is_sprinting = false
+	#else:
+		#if !sprint_toggle:
+			#if Input.is_action_pressed("sprint"): # && (!is_crouching || can_uncrouch()):
+				#if can_crouch_sprint:
+					#is_sprinting = true
+				#else:
+					#if !is_crouching || can_uncrouch():
+						#try_uncrouch()
+						#is_sprinting = true
+			#else: 
+				#is_sprinting = false
+	#
+	#if is_crouching && on_floor:
+		#speed_mult = crouch_speed_mult
+	#else:
+		#speed_mult = normal_speed_mult
+	#
+	#if is_sprinting:
+		#speed_mult *= sprint_speed_mult
+
+	# Get the input direction and handle the movement/deceleration.
+	
+	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (%CameraRotationRoot.transform.basis * Vector3(input_dir.x, 0, input_dir.y))
+	direction = Vector3(direction.x, 0, direction.z).normalized()
+	
+	if direction:
+		#if ladder != null:
+			#move_on_ladder(input_dir, direction, delta)
+		#else: move(direction, delta, on_floor)
+		move(direction, delta, on_floor)
+		
+	else:
+		if on_floor:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
+		#if ladder != null:
+			#if is_jumping:
+				#velocity.y -= gravity * delta
+				#if velocity.y < 0:
+					#is_jumping = false
+			#else: velocity.y = move_toward(velocity.y, 0, SPEED)
+		#
+		#is_clambering = false
+		#
+		#if temp_crouch && is_crouching:
+			#try_uncrouch()
+			#
+		#update_head_bob(delta, landing_head_bob)
+
+	move_and_slide()
+	
+	#last_velocity = velocity
+
+
 func update_camera(delta: float, rotation_input: float, tilt_input: float):
 	mouse_rotation.x += tilt_input * delta
 	mouse_rotation.x = clamp(mouse_rotation.x, tilt_lower_limit, tilt_upper_limit)
@@ -40,3 +157,51 @@ func update_camera(delta: float, rotation_input: float, tilt_input: float):
 	
 	rotation_input = 0.0
 	tilt_input = 0.0
+
+
+func move(direction: Vector3, delta, on_floor: bool):
+	#rotate shapecast_move_root in direction of movement
+	#shapecast_move_root.look_at(shapecast_move_root.global_position + Vector3(direction.x, 0, direction.z))
+	#
+	#var camera_forward = camera_rotation_root.transform.basis * Vector3.FORWARD
+	#var camera_tilt_dot = camera_forward.dot(Vector3.UP)
+	var xz_move = accelerate_in_direction(direction, delta, speed_mult)
+	velocity.x = xz_move.x
+	velocity.z = xz_move.z
+	
+	#var stepping_up = step_detect()
+	#if stepping_up:
+		#velocity.y = step_up_speed * delta * speed_mult
+		#on_steps = true
+		##stepping_up = true
+		##update_head_bob(delta, on_floor)
+		##return
+		#
+	#if !is_crouching: #check if temp crouch needed
+		#camera_forward.y = 0
+		#var camera_move_dot = camera_forward.normalized().dot(direction)
+		#if camera_move_dot > 0.8:
+			#if on_floor:
+				#if auto_crouch && camera_tilt_dot < -0.45 && !shapecast_look_legs.is_colliding() && shapecast_look_head.is_colliding():
+					#temp_crouch = true
+					#crouch()
+			#else:
+				#if !stepping_up && shapecast_look_legs.is_colliding() && !shapecast_look_head.is_colliding():
+					#temp_crouch = true
+					#crouch()
+	#else:
+		#if temp_crouch && !shapecast_look_legs.is_colliding() && !shapecast_look_head.is_colliding():
+			#try_uncrouch()
+	#
+	#if camera_tilt_dot > 0 && ledge_detect() && !is_jumping && !on_floor:
+		#is_clambering = true
+		#velocity.y = clamber_speed * delta
+	#else: is_clambering = false
+		#
+	#update_head_bob(delta, on_floor || stepping_up)
+
+
+func accelerate_in_direction(direction: Vector3, delta: float, mult: float) -> Vector3:
+	var desired_velocity: Vector3 = direction * SPEED * mult
+	var adjusted_velocity = velocity.lerp(desired_velocity, 1 - exp(-acceleration * delta))
+	return adjusted_velocity#Vector2(adjusted_velocity.x, adjusted_velocity.z)
